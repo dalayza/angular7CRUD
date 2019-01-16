28c17af8d5c23f19f64dc8a4d0072b02d6f5cb83
Denis Alayza <denis.alayza@gmail.com>
2019-01-16 02:14:35 -0200

    App CRUD

---
+++ server.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var mongo = require('mongoose');
+
+var db = mongo.connect("mongodb://denis_alayza:caracas01@ds255262.mlab.com:55262/star_wars", function(err, response){
+    if(err){ console.log(err); }
+    else{ console.log('Connected to ' + db, ' + ', response ); }
+});
+
+var app = express()
+app.use(bodyParser());
+app.use(bodyParser.json({ limit:'5mb' }));
+app.use(bodyParser.json({ extended:true }));
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin','http://localhost/4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Rquested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+var Schema = mongo.Schema;
+
+var UsersSchema = new Schema({
+    name: { type: String },
+    address: { type: String },
+},{ versionKey: false });
+
+var model = mongo.model('users', UsersSchema, 'users');
+
+app.post("/api/SaveUser", function(req, res){
+    var mod = new model(req.body);
+    mod.save(function(err, data){
+        if(err){
+            res.send(err);
+        }
+        else {
+            res.send({ data: "Record has been Inserted..!!" });
+        }
+    });
+})
+
+app.post("/api/UpdateUser", function(req, res){
+    var mod = new model(req.body);
+    model.findByIdAndUpdate(req.body._id, { name: req.body.name, address: req.body.address },
+    function(err, data){
+        if(err){
+            res.send(err);
+        }
+        else {
+            res.send({ data: "Record has been Updated..!!" });
+        }
+    });
+})
+
+app.post("/api/deleteUser", function(req, res){
+    model.remove({ _id: req.body.id }, function(err) {
+        if(err){
+            res.send(err);
+        }
+        else {
+            res.send({ data: "Record has been Deleted..!!" });
+        }
+    });
+})
+
+app.get("/api/getUser", function(req, res){
+    model.find({}, function(err, data) {
+        if(err){
+            res.send(err);
+        }
+        else {
+            res.send(data);
+        }
+    });
+})
+
+app.listen(8080, function() {
+    console.log('Example app listening on port 8080!');
+})
